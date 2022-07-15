c53274475a803b167262a61bb7dda57cb48cac03
hhchinh2002 <64810838+hhchinh2002@users.noreply.github.com>
2022-07-16 05:02:03 +0800

    make it better

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "password",
-    database: "socialmedia",
+    host: "us-cdbr-east-06.cleardb.net",
+    user: "be63cf0790b8a2",
+    password: "a65f2952",
+    database: "heroku_99140c5a2371a57",
 });
 
 const userRoute = require('./routes/User')
