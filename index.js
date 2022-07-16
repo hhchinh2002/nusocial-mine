b532accc8016eb8be1b650f8bd9ad0a24db0f585
hhchinh2002 <64810838+hhchinh2002@users.noreply.github.com>
2022-07-16 17:15:37 +0800

    make it better

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ app.use('', userRoute);
 const postingRoute = require('./routes/Posting')
 app.use('', postingRoute)
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
-app.listen(process.env.PORT || PORT, (req, res) => {
-    console.log("Server is running on port " + PORT.toString());
+app.listen(PORT, (req, res) => {
+    console.log("Server is running on port " + PORT);
 });
