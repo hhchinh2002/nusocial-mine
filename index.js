4dfc54601ba749e62a0a2330207698d5b1a2f0c5
hhchinh2002 <64810838+hhchinh2002@users.noreply.github.com>
2022-07-17 21:32:26 +0800

    make it better

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mysql = require("mysql");
 const cors = require("cors");
+const path = require("path");
 
 app.use(cors());
 app.use(express.json());
